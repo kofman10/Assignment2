c28ffbcdf54002f530d8b5cc8aa94b3348cbae4e
kofman10 <kajotonis@gmail.com>
2023-10-15 22:52:51 +0100

    final push

--- assignment.sh
+++ assignment.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# ssh into master and create a user named altschool and assign it super user privileges create a file in a directory and copy into thesame directo
+# on slave node.. this should be performed from the master node 
+
+# The Master node should display an overview of the Linux process management, showcasing currently running processes.
 # Vagrant box names for Ubuntu
+
 MASTER_BOX="ubuntu/bionic64"
 SLAVE_BOX="ubuntu/bionic64"
 
@@ -15,14 +20,15 @@ create_vagrant_environment() {
 Vagrant.configure("2") do |config|
   config.vm.define "master" do |subconfig|
     subconfig.vm.box = "$MASTER_BOX"
-    subconfig.vm.hostname = "masters"
-    subconfig.vm.network "private_network", type: "dhcp"
+    subconfig.vm.hostname = "master"
+    subconfig.vm.network "private_network", type: "static", ip: "192.168.56.10"
+    subconfig.vm.provision "shell", inline: "sudo useradd -m -s /bin/bash altschool"
   end
 
   config.vm.define "slave" do |subconfig|
     subconfig.vm.box = "$SLAVE_BOX"
     subconfig.vm.hostname = "slave"
-    subconfig.vm.network "private_network", type: "dhcp"
+    subconfig.vm.network "private_network", type: "static", ip: "192.168.56.11"
   end
 
   #Provisioning script
@@ -34,6 +40,12 @@ EOF
 }
 
 create_vagrant_environment
+
+vagrant ssh master
+
+ssh-keygen -t rsa
+
+ssh-copy-id vagrant@192.168.56.11
 
 # Output instructions to SSH into the 'Slave' node
 echo "You can SSH into the 'Slave' node using 'vagrant ssh slave' after provisioning."
--- script.sh
+++ script.sh
@@ -9,28 +9,26 @@
 # on slave node.. this should be performed from the master node 
 
 # The Master node should display an overview of the Linux process management, showcasing currently running processes.
-mysql_root_password="password"
+
 
 function install_lamp_stack {
     # Install LAMP Stack
     sudo apt-get update
-    sudo apt-get install -y apache2 mysql-server php8.1 libapache2-mod-php 
+    sudo apt-get install -y apache2 mysql-server php libapache2-mod-php 
     sudo systemctl enable apache2
     sudo systemctl start apache2
-    sudo mysql_secure_installation 
+    sudo mysql -e "ALTER USER 'root'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'password';"
+    sudo mysql -u root -p 'password' -e "CREATE USER 'username'@'host' IDENTIFIED WITH authentication_plugin BY 'password';"
 }
 
 # Install LAMP stack on both sides
 install_lamp_stack
 echo "lamp stack installed"
-    sudo mysql -u root -p$mysql_root_password -e "CREATE USER 'myuser'@'localhost' IDENTIFIED BY 'mypassword';"
+   
 
 # Test PHP functionality
 echo "<?php phpinfo(); ?>" | sudo tee /var/www/html/info.php
 
 echo "LAMP stack installed. You can access PHP info at http://localhost/info.php"
 
-# allow machines to communicate with themselves via hostname
-sudo apt install -y avahi-daemon libnss-mdns
 
-echo "machines can now talk to themselves on to the next ....."
